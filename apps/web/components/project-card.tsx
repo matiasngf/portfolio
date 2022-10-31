7a4a1ed9f958610455fa4dccaf80de132ec7df93
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-10-31 20:19:58 -0300

    update card style

--- apps/web/components/project-card.tsx
+++ apps/web/components/project-card.tsx
@@ -11,9 +11,9 @@ export const ProjectCard = ({
   project
 }: ProjectCardProps) => (
   <Link href={`/projects/${projectKey}`}>
-    <div className=' space-y-1 text-slate-300 p-4 rounded-md border border-slate-600 md:hover:scale-105 transition-all'>
-      <h2 className='text-lg'>{project.name}</h2>
-      <p>{project.description}</p>
+    <div className=' space-y-1 text-slate-300 p-4 rounded-md border border-slate-800 md:hover:scale-105 transition-all'>
+      <h2 className='text-lg font-semibold'>{project.name}</h2>
+      <p className='text-slate-400'>{project.description}</p>
       <div className='pt-4 flex flex-wrap space-x-4'>
         {project.tags.map((tag) => (
           <Tag key={tag} tag={tag} />
