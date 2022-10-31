a48094cb7a567d365f911079b47f85d1a637eb67
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2022-10-31 19:29:13 -0300

    Add source prop

--- apps/web/utils/projects-config.ts
+++ apps/web/utils/projects-config.ts
@@ -5,8 +5,11 @@ export interface ProjectConfig {
   name: string;
   description: string;
   load: DynamicProjectLoader
+  source: string;
   tags: TagKey[]
 }
+
+const baseExperiment = 'https://github.com/matiasngf/portfolio/tree/main/packages/experiments'
 
 export interface Projects {
   [key: string]: ProjectConfig;
@@ -17,24 +20,28 @@ export const projectsConfig: Projects = {
     name: 'ThreeJS Simple Scene',
     description: 'A simple scene renderer with vanilla threeJs.',
     load: loadDynamicProject(() => import('experiments-simple-scene')),
+    source: `${baseExperiment}/simple-scene`,
     tags: ['open-gl', 'shaders']
   },
   'ray-marching-simple': {
     name: 'Ray Marching Basic',
     description: 'Base ray marching renderer.',
     load: loadDynamicProject(() => import('experiments-ray-marching-simple')),
+    source: `${baseExperiment}/ray-marching/simple`,
     tags: ['open-gl', 'shaders', 'ray-marching']
   },
   'ray-marching-boolean-opeartions': {
     name: 'Boolean operaions',
     description: 'Operations with objetcs in a ray-marching renderer.',
     load: loadDynamicProject(() => import('experiments-ray-marching-boolean-opeartions')),
+    source: `${baseExperiment}/ray-marching/boolean-operations`,
     tags: ['open-gl', 'shaders', 'ray-marching']
   },
   'ray-marching-reflections': {
     name: 'Real time reflections',
-    description: 'Reflections calculated using ray-marching on real-time.',
+    description: 'Reflections calculated using ray-marching on real time.',
     load: loadDynamicProject(() => import('experiments-ray-marching-reflections')),
+    source: `${baseExperiment}/ray-marching/reflections`,
     tags: ['open-gl', 'shaders', 'ray-marching']
   },
 }
