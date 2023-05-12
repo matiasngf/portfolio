e9adb0fe78cbd477a18649e2e57a0b4bf2d8f857
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2023-05-12 12:36:02 -0300

    add uTime uniform

--- packages/experiments/earth/src/Earth/index.tsx
+++ packages/experiments/earth/src/Earth/index.tsx
@@ -1,17 +1,19 @@
 import { Sphere } from "@react-three/drei";
 import { SRGBColorSpace, Vector3 } from "three";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import { useLoader } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 
 import earthDayMapUrl from "./textures/8k_earth_daymap.jpg";
 import nightMapUrl from "./textures/8k_earth_nightmap.jpg";
 import cloudMapUrl from "./textures/8k_earth_clouds.jpg";
 
 import { earthFragmentShader, earthVertexShader } from "./shaders";
+import { useRef } from "react";
 
-export const lightDirectionRaw = new Vector3(1, 0, 0);
-export const lightDirection = lightDirectionRaw;
-// .applyAxisAngle(new Vector3(0, 0, 1), Math.PI * (13 / 180));
+export const lightDirection = new Vector3(1, 0, 0).applyAxisAngle(
+  new Vector3(0, 0, 1),
+  Math.PI * (13 / 180)
+);
 
 const verteces = Math.pow(2, 9);
 
@@ -26,9 +28,16 @@ export const Earth = () => {
     cloudTexture.colorSpace =
       SRGBColorSpace;
 
+  const matRef = useRef(null);
+
+  useFrame((_, delta) => {
+    matRef.current.uniforms.uTime.value += delta;
+  });
+
   return (
     <Sphere args={[1, verteces, verteces]}>
       <shaderMaterial
+        ref={matRef}
         vertexShader={earthVertexShader}
         fragmentShader={earthFragmentShader}
         uniforms={{
@@ -36,6 +45,7 @@ export const Earth = () => {
           nightMap: { value: nightTexture },
           cloudMap: { value: cloudTexture },
           lightDirection: { value: lightDirection },
+          uTime: { value: 0 },
         }}
       />
     </Sphere>
