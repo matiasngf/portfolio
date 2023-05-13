6e6dca8cb8ace6e0eda1f7d6b932a043ea5a654b
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2023-05-13 20:07:02 -0300

    delete unused imports

--- packages/experiments/earth/src/index.tsx
+++ packages/experiments/earth/src/index.tsx
@@ -1,7 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 import { PrimaryScene } from "./PrimaryScene";
 import { Effects } from "./Effects";
-import { Html, Loader, useProgress } from "@react-three/drei";
+import { Html } from "@react-three/drei";
 import { Suspense } from "react";
 
 function Loading() {
