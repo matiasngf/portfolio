395e4c18556c98bbda354bfe4c39d5f588008673
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2023-06-23 18:08:54 -0300

    change background for dev

--- packages/experiments/shaders/plants/src/PrimaryScene/index.tsx
+++ packages/experiments/shaders/plants/src/PrimaryScene/index.tsx
@@ -1,8 +1,4 @@
-import { useThree } from "@react-three/fiber";
-import { PropsWithChildren, useEffect, useState } from "react";
-import { SRGBColorSpace, TextureLoader, WebGLCubeRenderTarget } from "three";
-import { Bounds } from "@react-three/drei";
-import { useBackgroundStore } from "./background";
+import { PropsWithChildren } from "react";
 import { Plant } from "../Plant";
 
 export interface PrimarySceneProps {}
@@ -10,34 +6,12 @@ export interface PrimarySceneProps {}
 export const PrimaryScene = ({
   children,
 }: PropsWithChildren<PrimarySceneProps>) => {
-  // global texture
-
-  const cubeTexture = useBackgroundStore((s) => s.texture);
-  const setTexture = useBackgroundStore((s) => s.setTexture);
-
-  const { gl, scene } = useThree();
-
-  useEffect(() => {
-    const loader = new TextureLoader();
-    loader.load("/experiment-shaders-fluid-assets/woods_4k.jpg", (texture) => {
-      texture.colorSpace = SRGBColorSpace;
-      const cubeRenderTarget = new WebGLCubeRenderTarget(texture.image.height);
-      cubeRenderTarget.fromEquirectangularTexture(gl, texture);
-      setTexture(cubeRenderTarget.texture);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (cubeTexture) {
-      scene.background = cubeTexture;
-    }
-  }, [cubeTexture]);
-
   return (
     <>
       {children}
       <Plant />
       <ambientLight />
+      <color attach="background" args={["#111"]} />
     </>
   );
 };
--- packages/experiments/shaders/plants/src/index.tsx
+++ packages/experiments/shaders/plants/src/index.tsx
@@ -30,13 +30,17 @@ const ThreeApp = () => {
     <>
       {debugGrid && (
         <>
-          <gridHelper renderOrder={1} args={[10, 10]} />
+          <gridHelper renderOrder={1} args={[2, 20, "#000", "#333"]} />
           <gridHelper
             renderOrder={1}
             rotation={[Math.PI / 2, 0, 0]}
-            args={[10, 10]}
+            args={[2, 20, "#000", "#333"]}
           />
-          <axesHelper renderOrder={2} args={[10]} />
+          <axesHelper
+            renderOrder={2}
+            args={[1]}
+            position={[0.001, 0.001, 0.001]}
+          />
         </>
       )}
       <PrimaryScene />
@@ -50,7 +54,7 @@ const App = () => {
       onCreated={(state) => {
         state.camera.lookAt(0, 2, 0);
       }}
-      camera={{ fov: 40, position: [2, 2, 2] }}
+      camera={{ fov: 40, position: [2, 2, 2], near: 0.01, far: 20 }}
     >
       <ThreeApp />
       <OrbitControls />
