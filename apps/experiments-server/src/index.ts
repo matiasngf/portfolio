cc205ac74003a2a7b955efa59fffb2b48a7c85c1
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-05-06 23:53:56 -0300

    fix experiment routes

--- apps/experiments-server/src/index.ts
+++ apps/experiments-server/src/index.ts
@@ -1,16 +1,16 @@
 import express from 'express'
 import path from 'path'
 
-const app = express()
+const app: express.Application = express()
 const experimentsPath = path.dirname(process.argv[1])
 
 // Serve static files from the experiments directory
-app.use('/:slug', (req, res, next) => {
+app.use('/experiments/:slug', (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const slug = req.params.slug
   const experimentPath = path.join(experimentsPath, slug)
 
   // First try to serve index.html for the root path
-  if (req.path === '/' || req.path === '') {
+  if (req.path === '/experiments') {
     express.static(experimentPath)(req, res, next)
   } else {
     // Then try to serve any file within the experiment directory
@@ -24,11 +24,11 @@ app.use((req, res, next) => {
 
   if (referer) {
     // Extract experiment slug from referer URL
-    const match = referer.match(/\/([^\/]+)/)
+    const match = referer.match(/\/experiments\/([^\/]+)/)
 
     if (match && match[1]) {
       const slug = match[1]
-      const redirectPath = `/${slug}${req.path}`
+      const redirectPath = `/experiments/${slug}${req.path}`
 
       // Redirect to the experiment's path
       return res.redirect(redirectPath)
@@ -48,4 +48,4 @@ app.listen(3000, () => {
   console.log(`Server started at http://localhost:3000`)
 })
 
-module.exports = app
+export default app
