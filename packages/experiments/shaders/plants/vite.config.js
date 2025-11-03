c522c3e578ba98b1b6c59894dce98ad652e22571
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2025-11-02 21:27:24 -0300

    update configs

--- packages/experiments/shaders/plants/vite.config.js
+++ packages/experiments/shaders/plants/vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
 
-export default defineConfig({
+const experimentConfig = JSON.parse(
+  readFileSync(resolve(process.cwd(), 'experiment-config.json'), 'utf-8')
+)
+
+const basePath = `/${experimentConfig.name}`
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? basePath : '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    target: 'esnext'
+  },
+  esbuild: {
+    target: 'esnext'
+  },
   plugins: [react()]
-})
+}))
+
