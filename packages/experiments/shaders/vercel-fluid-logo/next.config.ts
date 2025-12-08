df7069ce2a00247e06df2a5a7a8126839d4cd20b
matiasngf <matiasngf@hotmail.com>
2025-12-08 10:34:27 -0300

    update base path

--- packages/experiments/shaders/vercel-fluid-logo/next.config.ts
+++ packages/experiments/shaders/vercel-fluid-logo/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/vercel-fluid-logo",
 };
 
 export default nextConfig;
